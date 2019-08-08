vhqtvn/pam-fprint-grosshack

commit 3274a31153e9b730ec04874c61fc5119df6eb46b

pam: Don't ask for fingerprints for remote logins
As written in the "Linux-PAM Application Developers' Guide"
at http://www.linux-pam.org/Linux-PAM-html/adg-security-user-identity.html:
"
As a general rule, the following convention for its value can be
assumed: NULL = unknown; localhost = invoked directly from the
local system; other.place.xyz = some component of the user's
connection originates from this remote/requesting host.
"

So also exit early if the hostname isn't localhost as it should be.

Closes: #21

pam/pam_fprintd.c
@@ -476,7 +476,13 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
 					   G_TYPE_NONE, G_TYPE_STRING, G_TYPE_BOOLEAN, G_TYPE_INVALID);
 
 	pam_get_item(pamh, PAM_RHOST, (const void **)(const void*) &rhost);
-	if (rhost != NULL && strlen(rhost) > 0) {
+
+	if (rhost == NULL || *rhost == '\0') {
+		/* unavailable host information */
+		return PAM_AUTHINFO_UNAVAIL;
+	}
+
+	if (strcmp (rhost, "localhost") != 0) {
 		/* remote login (e.g. over SSH) */
 		return PAM_AUTHINFO_UNAVAIL;
 	}

commit 8f90390c6b215523f280452042a8844bb1115aca

pam: Fix rhost check when rhost is unavailable
Don't exit early if the rhost field isn't filled in, as it usually isn't
for a lot of services.

Fixes: 3274a31

pam/pam_fprintd.c
@@ -477,13 +477,12 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
 
 	pam_get_item(pamh, PAM_RHOST, (const void **)(const void*) &rhost);
 
-	if (rhost == NULL || *rhost == '\0') {
-		/* unavailable host information */
-		return PAM_AUTHINFO_UNAVAIL;
-	}
-
-	if (strcmp (rhost, "localhost") != 0) {
-		/* remote login (e.g. over SSH) */
+	/* NULL or empty rhost if the host information is not available or set.
+	 * "localhost" if the host is local.
+	 * We want to not run for known remote hosts */
+	if (rhost != NULL &&
+	    rhost != '\0' &&
+	    strcmp (rhost, "localhost") != 0) {
 		return PAM_AUTHINFO_UNAVAIL;
 	}
 