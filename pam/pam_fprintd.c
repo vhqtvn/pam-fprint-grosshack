vhqtvn/pam-fprint-grosshack

commit affffaf134f52dc01fbdb66b81172d983fbf74b2

pam: Return PAM_MAXTRIES after too many tries

pam/pam_fprintd.c
@@ -433,6 +433,9 @@ do_verify (pam_handle_t *pamh,
 		data->max_tries--;
 	}
 
+	if (data->max_tries == 0)
+		ret = PAM_MAXTRIES;
+
 	sd_bus_slot_unref (verify_status_slot);
 	sd_bus_slot_unref (verify_finger_selected_slot);
 

tests/pam/test_pam_fprintd.py
@@ -29,6 +29,7 @@ PAM_SUCCESS = 0
 PAM_AUTH_ERR = 7
 PAM_AUTHINFO_UNAVAIL = 9
 PAM_USER_UNKNOWN = 10
+PAM_MAXTRIES = 11
 
 class TestPamFprintd(dbusmock.DBusTestCase):
     '''Test pam_fprintd'''
@@ -127,7 +128,7 @@ class TestPamFprintd(dbusmock.DBusTestCase):
         ]
         self.device_mock.SetVerifyScript(script)
 
-        tc = pypamtest.TestCase(pypamtest.PAMTEST_AUTHENTICATE, expected_rv=PAM_AUTH_ERR)
+        tc = pypamtest.TestCase(pypamtest.PAMTEST_AUTHENTICATE, expected_rv=PAM_MAXTRIES)
         res = pypamtest.run_pamtest("toto", "fprintd-pam-test", [tc], [ 'unused' ])
 
         self.assertRegex(res.info[0], r'Swipe your left little finger across the fingerprint reader')

commit f745c49112d1638967070cf52d1c1a7bc85ce5d1

pam: Fix PAM authentication when it's the last try
We need to make sure that the max_tries variable isn't decremented
further when we have success in the verification loop. Add missing break
to do that.

Fixes: affffaf134f52dc01fbdb66b81172d983fbf74b2
Closes: #40

pam/pam_fprintd.c
@@ -417,6 +417,7 @@ do_verify (pam_handle_t *pamh,
 				ret = PAM_AUTH_ERR;
 			} else if (str_equal (data->result, "verify-match")) {
 				ret = PAM_SUCCESS;
+				break;
 			} else if (str_equal (data->result, "verify-unknown-error")) {
 				ret = PAM_AUTHINFO_UNAVAIL;
 			} else if (str_equal (data->result, "verify-disconnected")) {