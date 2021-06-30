0d2dbc617970a1aa4ff73bc1b4e2aa546bd2b84d
Benjamin Berg <bberg@redhat.com>
2021-06-30 14:17:28 +0200

    Switch to the Fedora weblate instance for translations
    Closes: #78

--- update-transifex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-test -f .tx/config || exit 1
-
-echo Pulling translations from Transifex
-tx --root `dirname $0` pull --all --force --skip
-
-echo Pushing strings to Transifex
-tx push --source
